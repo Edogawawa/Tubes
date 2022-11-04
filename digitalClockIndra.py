a5a9a4801edbfdb37946b126bb420eefd9392434
Beddds <fathoniubay@gmail.com>
2022-11-04 15:56:31 +0700

    penambahan

--- digitalClockIndra.py
+++ digitalClockIndra.py
@@ -2,26 +2,54 @@ import time
 from sys import *
 from tkinter import *
 
-clockview = Tk()
-clockview.title("Digital Clock")
-clockview.geometry("1000x750")
-clockview.configure(bg='black')
+def view():
+    clockview = Tk()
+    clockview.title("Digital Clock")
+    clockview.geometry("1000x750")
+    clockview.configure(bg='black')
+    
+    def Timer():
+        timer = Tk()
+        timer.title("Digital timer")
+        timer.geometry("750x400")
+        timer.configure(bg="black")
+        timer.mainloop()
 
-def get_time():
-    timeVar = time.strftime("%H:%M:%S", time.localtime())
-    clock.config(text = timeVar)
-    clock.after(1000, get_time)
+    def Stopwatch():
+        clockview.destroy()
+        stopwatch = Tk()
+        stopwatch.title("Stopwatch")
+        stopwatch.geometry("750x400")
+        stopwatch.configure(bg="black")
+        
+        stopwatch.mainloop()
 
-clock = Label(clockview, font=("Helvetica", 80), bg="black", fg="white")
-clock.place(x = 265, y = 300)
-get_time()
+    def Alarm():
+        clockview.destroy()
+        alarm = Tk()
+        alarm.title("Alarm")
+        alarm.geometry("750x400")
+        alarm.configure(bg="black")
+        
+        alarm.mainloop()
 
+    def get_time():
+        timeVar = time.strftime("%H:%M:%S", time.localtime())
+        clock.config(text = timeVar)
+        clock.after(1000, get_time)
+        
+    clock = Label(clockview, font=("Helvetica", 80), bg="black", fg="white")
+    clock.place(x = 265, y = 300)
+    get_time()
 
-f=Frame(clockview,bg="black",highlightbackground="black",highlightthickness=1,width=985,height=100)
-Button(f, font=("Arial", 20, 'bold'), text="Timer", fg="black", bg="grey").place(x=5, y=10)
-Button(f, font=("Arial", 20, 'bold'), text="Alarm", fg="black", bg="grey").place(x=110, y=10)
-Button(f, font=("Arial", 20, 'bold'), text="Stopwatch", fg="black", bg="grey").place(x=216, y=10)
-f.place(x = 5, y = 5)
+    f=Frame(clockview,bg="black",highlightbackground="black",highlightthickness=1,width=985,height=100)
+    Button(f, font=("Arial", 16, 'bold'), text="Timer", fg="black", bg="grey", command=Timer).place(x=4, y=4)
+    Button(f, font=("Arial", 16, 'bold'), text="Alarm", fg="black", bg="grey", command=Alarm).place(x=84, y=4)
+    Button(f, font=("Arial", 16, 'bold'), text="Stopwatch", fg="black", bg="grey", command=Stopwatch).place(x=166, y=4)
+    
+    f.place(x = 5, y = 5)
 
-clockview.mainloop()
+    clockview.mainloop()
 
+view()
+    
